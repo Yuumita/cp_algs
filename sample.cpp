451f56f9c22ffbf146bda4a0e9920d05017cdedf
Yuumita <47950940+Yuumita@users.noreply.github.com>
2022-02-12 00:04:32 +0200

    Add files via upload

---
+++ sample.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb  push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define forn(i,l,r) for(int i=l; i<r; i++)
+const int MOD = 1e9 + 7, INF = 2e9+5, NN = 2e6;
+
+void solve(){
+
+}
+ 
+int main(){
+	// ios_base::sync_with_stdio(0); cin.tie(NULL);
+	int t = 1;
+	//cin >> t;
+	for(int tt=0;tt<t;tt++){
+		//cout <<"Test Case #" << tt << ":\n";
+		solve();
+		cout << endl;
+	}
+}
