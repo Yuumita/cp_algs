52275f77ec99ee5e1fe008b01db003a9ca7a4e5d
Yuumita <operdarkgr@gmail.com>
2022-09-15 23:33:58 +0300

    Added the basic geometry library, "point".

---
+++ graphs/find_bridges.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// TODO: make edges a structure with ids etc.
+struct find_bridges { 
+    int n;
+    vector<vector<int>> adj;
+    vector<int> tin, low, edges;
+    vector<bool> visited, bridge;
+
+    find_bridges(int _n = 0) {
+        init(_n);
+    }
+
+    void init(int _n) {
+        n = _n;
+        adj.assign(n, {});
+        visited.assign(n, false);
+        tin.assign(n, -1);
+        low.assign(n, n+1);
+        bridge.assign(n, false);
+    }
+
+    // ensure u --> v is an edge beforehand
+    bool is_bridge(int u, int v) { 
+        return low[v] > tin[u];
+    }
+
+    void add_edge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    void dfs(int u, int &t, int p = -1) {
+        visited[u] = true;
+        tin[u] = low[u] = t++;
+        for(int v: adj[u]) {
+            if(v == p) continue;
+            if(visited[v]) {
+                low[u] = min(low[u], tin[v]);
+            } else { 
+                dfs(v, t, u);
+                low[u] = min(low[u], low[v]);
+                
+                // TODO: change this
+                if(is_bridge(u, v)) {
+                    cout <<"Bridge: " << u <<" " << v << endl;
+                }
+            }
+        }
+    }
+
+    void build() {
+        int t = 0; 
+        init(n); 
+        for(int i = 0; i < n; i++)
+            if(!visited[i])
+                dfs(i, t);
+    }
+
+};
+ 
+int main(){
+
+}
--- template.cpp
+++ template.cpp
@@ -1,11 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
 #define pb  push_back
 #define mp make_pair
 #define all(x) x.begin(), x.end()
-#define forn(i,l,r) for(int i=l; i<r; i++)
-const int MOD = 1e9 + 7, INF = 2e9+5, NN = 2e6;
+#define forn(i,l,r) for(int (i)=(l); (i)<(r); (i)++)
+#define rinc(i,l,r) for(int (i)=(l); (i)<=(r); (i)++)
+#define rdec(i,r,l) for(int (i)=(r); (i)>=(l); (i)--)
+#define deb(x) cerr<<#x<<" = "<<x<<endl;
+const int MOD = 1e9 + 7; // 998244353
+const int INF = 2e9+5; 
+const int MAX = 2e5 + 7;
+
+template<class T> 
+bool ckmin(T &a, const T &b) { return b < a ? a = b, true : false; } // a := min(a, b)
+template<class T> 
+bool ckmax(T &a, const T &b) { return b > a ? a = b, true : false; } // a := min(a, b)
+
 
 void solve(){
 
